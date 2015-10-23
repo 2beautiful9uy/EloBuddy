adda0ecf9de1b9f5266b9ad076956ef7b1918d4c
funboxxx <funboxxx@wp.pl>
2015-10-23 16:34:29 +0100

    123

--- ParanoidOrbwalking/ParanoidOrbwalking/Program.cs
+++ ParanoidOrbwalking/ParanoidOrbwalking/Program.cs
@@ -32,7 +32,9 @@ namespace ParanoidOrbwalking
         static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             if (sender.IsMe)
+            {
                 lastaa = Game.Time * 1000;
+            }
         }
 
         static void Game_OnTick(EventArgs args)
@@ -43,30 +45,46 @@ namespace ParanoidOrbwalking
                 switch (ObjectManager.Player.ChampionName.ToLower())
                 {
                     case "kalista":
+                    {
                         switch (target.IsValidTarget() && !target.IsZombie)
                         {
                             case true:
+                            {
                                 switch (Game.Time * 1000 > lastaa + ObjectManager.Player.AttackDelay * 1000 - 150f)
                                 {
                                     case true:
+                                    {
                                         Player.IssueOrder(GameObjectOrder.AttackUnit, target);
+                                    }
                                     break;
                                     case false:
+                                    {
                                         if (Game.Time * 1000 > lastaa + 1)
+                                        {
                                             Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                                        }
+                                    }
                                     break;
                                 }
+                            }
                             break;
                             case false:
+                            {
                                 Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                            }
                             break;
                         }
+                    }
                     break;
                     case "ezreal":
+                    {
                         Orb(100f, 150f, target);
+                    }
                     break;
                     case "vayne":
+                    {
                         Orb(80f, 150f, target);
+                    }
                     break;
                 }
             }
@@ -77,19 +95,29 @@ namespace ParanoidOrbwalking
             switch (unit.IsValidTarget() && !unit.IsZombie)
             {
                 case true:
+                {
                     switch (Game.Time * 1000 > lastaa + ObjectManager.Player.AttackDelay * 1000 - attack)
                     {
                         case true:
+                        {
                             Player.IssueOrder(GameObjectOrder.AttackUnit, unit);
+                        }
                         break;
                         case false:
+                        {
                             if (Game.Time * 1000 > lastaa + ObjectManager.Player.AttackCastDelay * 1000 - move)
+                            {
                                 Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                            }
+                        }
                         break;
                     }
+                }
                 break;
                 case false:
+                {
                     Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+                }
                 break;
             }
         }
